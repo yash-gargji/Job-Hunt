b17626e9a7b24c1253ef53a3659e222173437e12
Yash Garg <yash.gargji2004@gmail.com>
2025-05-30 03:57:51 +0530

    models some user controller

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+
+dotenv.config({});
+
 
 const app = express();
 
@@ -15,8 +20,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions)); 
 
-const PORT = 3000;
+const PORT = process.env.PORT ||  3000;
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server running at port ${PORT}`);
 });
 
