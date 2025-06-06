d76e754b931ea047c0ebc9395eefd3bd17eee828
Yash Garg <yash.gargji2004@gmail.com>
2025-06-07 05:06:07 +0530

    navbar

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="bg-white">
+      <div>
+        <h1 className="text-2xl font-bold">
+          Job<span className="text-[#F83002]">Portal</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
