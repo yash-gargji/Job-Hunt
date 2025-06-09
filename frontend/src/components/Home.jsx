c7fdc2ec793ca07effb66144cb923f6aa6a131e3
Yash Garg <yash.gargji2004@gmail.com>
2025-06-09 21:08:30 +0530

    signup adn inittial setup

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './shared/Navbar'
+
+function Home() {
+  return (
+    <Navbar/>
+  )
+}
+
+export default Home
--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,12 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
 
 function Navbar() {
+    const user = false;
   return (
     <div className="bg-white">
-      <div>
-        <h1 className="text-2xl font-bold">
-          Job<span className="text-[#F83002]">Portal</span>
-        </h1>
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job<span className="text-[#F83002]">Portal</span>
+          </h1>
+        </div>
+        <div className="flex items-center gap-12">
+          <ul className="flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+          </ul>
+          {!user ? (
+            <div className="flex items-center gap-2">
+              <Link to="/login">
+                <Button variant="outline">Login</Button>
+              </Link>
+              <Link to="/signup">
+                <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">
+                  Signup
+                </Button>
+              </Link>
+            </div>
+          ) : (
+           <Popover>
+            <PopoverTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage src="https://github.com/shadcn.png" />
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+                <h1>Hello</h1>
+            </PopoverContent>
+          </Popover>
+          )}
+        </div>
       </div>
     </div>
   );
